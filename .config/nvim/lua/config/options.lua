abe636d9c418cf5dce999315ca38979983f2afbe
Barr Israel <barr520@gmail.com>
2023-08-24 09:23:58 +0300

    dotfiles update

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,6 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 local opt = vim.opt
+opt.shell = "/usr/bin/fish"
 opt.clipboard = "unnamedplus"
 opt.tabstop = 4
 opt.softtabstop = 4
