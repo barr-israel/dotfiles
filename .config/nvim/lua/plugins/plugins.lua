1721071cba3e2dcab13c05c816e0ee7caef389c0
CattoFace <barr520@gmail.com>
2023-08-24 09:17:08 +0300

    dotfiles update

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -6,6 +6,38 @@ return {
   },
   {
     "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "debugloop/telescope-undo.nvim",
+        config = function()
+          require("telescope").load_extension("undo")
+        end,
+        keys = {
+          { "<C-u>", ":Telescope undo<CR>", desc = "Telescope Undo" },
+        },
+      },
+      {
+        "nvim-telescope/telescope-file-browser.nvim",
+        config = function()
+          require("telescope").load_extension("file_browser")
+        end,
+        keys = {
+          {
+            "<leader>e",
+            "<cmd>Telescope file_browser path=%:p:h select_buffer=true<CR>",
+            desc = "Telescope File Browser",
+          },
+        },
+      },
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+        config = function()
+          require("telescope").load_extension("fzf")
+        end,
+      },
+    },
     keys = {
       { "<leader>sb", "<cmd>Telescope buffers<cr>", desc = "Open Buffers" },
       { "<leader>sB", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Fuzzy Find In Buffer" },
@@ -29,13 +61,14 @@ return {
           hidden = true,
           prompt_path = true,
         },
+        undo = {
+          side_by_side = true,
+          layout_strategy = "vertical",
+          layout_config = {
+            preview_height = 0.8,
+          },
+        },
       },
-    },
-  },
-  {
-    "mbbill/undotree",
-    keys = {
-      { "<C-u>", "<cmd>UndotreeToggle<CR><cmd>UndotreeFocus<CR>", desc = "Toggle Undo Tree" },
     },
   },
   {
@@ -43,33 +76,23 @@ return {
     enabled = false,
   },
   {
-    "nvim-telescope/telescope-file-browser.nvim",
-    config = function()
-      require("telescope").load_extension("file_browser")
-    end,
-    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+    "echasnovski/mini.surround",
+    opts = {
+      mappings = {
+        add = "ss", -- Add surrounding in Normal and Visual modes
+        delete = "sd", -- Delete surrounding
+        find = "sf", -- Find surrounding (to the right)
+        find_left = "sF", -- Find surrounding (to the left)
+        highlight = "sh", -- Highlight surrounding
+        replace = "sr", -- Replace surrounding
+        update_n_lines = "sn", -- Update `n_lines`
+      },
+    },
+  },
+  {
+    "folke/flash.nvim",
     keys = {
-      { "<leader>e", "<cmd>Telescope file_browser path=%:p:h se;ect_buffer=true<CR>", desc = "Telescope File Browser" },
-    },
-    {
-      "echasnovski/mini.surround",
-      opts = {
-        mappings = {
-          add = "ss", -- Add surrounding in Normal and Visual modes
-          delete = "sd", -- Delete surrounding
-          find = "sf", -- Find surrounding (to the right)
-          find_left = "sF", -- Find surrounding (to the left)
-          highlight = "sh", -- Highlight surrounding
-          replace = "sr", -- Replace surrounding
-          update_n_lines = "sn", -- Update `n_lines`
-        },
-      },
-    },
-    {
-      "folke/flash.nvim",
-      keys = {
-        { "s", mode = { "n", "x", "o" }, false },
-      },
+      { "s", mode = { "n", "x", "o" }, false },
     },
   },
 }
