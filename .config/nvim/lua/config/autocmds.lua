cd32a0d5d91018d67fac94e5469b1e6780c602dc
Barr Israel <barr520@gmail.com>
2023-08-30 10:10:32 +0300

    dotfiles update

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,13 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+vim.api.nvim_create_autocmd({
+  "VimEnter",
+}, {
+  desc = "Auto select virtualenv Nvim open",
+  pattern = "*.pyx,*.py",
+  callback = function()
+    require("venv-selector").retrieve_from_cache()
+    vim.fn.setenv("PYTHONPATH", vim.fn.getenv("PWD"))
+  end,
+})
--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -95,4 +95,13 @@ return {
       { "s", mode = { "n", "x", "o" }, false },
     },
   },
+  {
+    "linux-cultist/venv-selector.nvim",
+    dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
+    opts = {
+      dap_enabled = true,
+      -- name = "venv",
+      -- auto_refresh = false
+    },
+  },
 }
