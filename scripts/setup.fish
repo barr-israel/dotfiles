70620008773aadd3b6f3ddcb90568f232045a31e
Barr <barr520@gmail.com>
2024-03-17 19:15:21 +0200

    dotfiles update

--- scripts/setup.fish
+++
@@ -1,5 +0,0 @@
-yay --noconfirm -S - < pkglist.txt
-curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
-omf install agnoster
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
