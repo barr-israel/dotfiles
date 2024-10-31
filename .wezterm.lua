4ccdb1d2b07f8954eed561f6bae1a606658aff6f
Barr <barr@barrisrael.com>
2024-10-31 17:00:56 +0200

    dotfiles update

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,6 +7,14 @@ config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
 -- config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
+
+wezterm.on("update-right-status", function(window, pane)
+	local cwd_uri = pane:get_current_working_dir()
+	local cwd = cwd_uri.file_path
+	-- local hostname = cwd_uri.host or wezterm.hostname()
+	local date = wezterm.strftime("%Y-%m-%d %H:%M:%S")
+	window:set_right_status(cwd .. " " .. date)
+end)
 config.keys = {
 	{
 		key = "|",
