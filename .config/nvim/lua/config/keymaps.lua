f57636e934025b00ec1a41e603dabc410f21e46f
Barr <barr@barrisrael.com>
2025-04-02 00:41:29 +0300

    dotfiles update

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,16 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+vim.keymap.set("n", "<C-w>", "<cmd>bd<CR>", { desc = "Close buffer" })
+vim.keymap.set("n", "<C-w>", "<cmd>bd<CR>", { desc = "Close buffer" })
+vim.keymap.set("v", "<C-Down>", ":m '>+1<CR>gv=gv", { desc = "Move Selection Down" })
+vim.keymap.set("v", "<C-Up>", ":m '<-2<CR>gv=gv", { desc = "Move Selection Up" })
+vim.keymap.set("n", "<C-Down>", ":m +1<CR>==", { desc = "Move Line Down" })
+vim.keymap.set("n", "<C-Up>", ":m -2<CR>==", { desc = "Move Line Up" })
+vim.keymap.set("i", "<C-Down>", "<C-o>:m +1<CR><C-o>==", { desc = "Move Line Down" })
+vim.keymap.set("i", "<C-Up>", "<C-o>:m -2<CR><C-o>==", { desc = "Move Line Up" })
+vim.keymap.set("n", "R", ":s/", { desc = "Replace in line" })
+vim.keymap.set("v", "R", 'y:%s/<c-r>"/', { desc = "Replace selection" })
+vim.keymap.set("n", "<c-r>", ":%s/", { desc = "Replace" })
+vim.keymap.set("v", "<c-r>", ":s/", { desc = "Replace in selection" })
+vim.keymap.set("n", "U", ":red<CR>", { desc = "Redo" })
---
+++ .config/nvim/lua/plugins/plugins.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    "mbbill/undotree",
+    config = function()
+      vim.g["undotree_SetFocusWhenToggle"] = 1
+      vim.g["undotree_WindowLayout"] = 2
+    end,
+    keys = {
+      { "<C-u>", "<cmd>UndotreeToggle<CR>", "Undo Tree Toggle" },
+    },
+  },
+  {
+    "akinsho/bufferline.nvim",
+    opts = {
+      options = {},
+    },
+    keys = {
+      { "B", mode = { "n", "v" }, require("bufferline").pick },
+    },
+  },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        sources = {
+          explorer = {
+            jump = { close = true },
+            layout = { preset = "default", preview = true },
+          },
+          files = {
+            layout = { preset = "default" },
+          },
+        },
+      },
+    },
+  },
+}
